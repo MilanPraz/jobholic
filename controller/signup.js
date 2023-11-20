cb0914611275fd4deeba5c22dccdc62ffe7d3a12
Milanpraz <mln.praz12@gmail.com>
2023-11-20 15:01:02 +0545

    ok

--- controller/signup.js
+++ controller/signup.js
@@ -35,6 +35,9 @@ const signup = async (req, res, next) => {
 
     console.log("req.body hai", req.body);
     console.log("req.file hai ", req.file);
+    if (req.body.image === "null") {
+      res.status(200).send("please add a image");
+    }
 
     const value = signupSchema.validate(req.body, {
       abortEarly: false,
